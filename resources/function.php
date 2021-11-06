f5d08887e5c17bff7838de66f342ca97249db77a
Rikoairlan <rikoairlan57@gmail.com>
2021-11-06 15:09:17 +0700

    create shop page

--- resources/function.php
+++ resources/function.php
@@ -43,13 +43,13 @@ function get_products() {
 
             <div class="col-sm-4 col-lg-4 col-md-4">
                 <div class="thumbnail">
-                    <a href="item.php?id={row['product_id']}"><img src="{$row['product_image']}" alt=""></a> 
+                    <a href="item.php?id={$row['product_id']}"><img src="{$row['product_image']}" alt=""></a>
                     <div class="caption">
                         <h4 class="pull-right">&#36;{$row['product_price']}</h4>
-                        <h4><a href="#">{$row['product_title']}</a>
+                        <h4><a href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
                         </h4>
                         <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <a class="btn btn-primary" target=_blank href="item.php?id={row['product_id']}">Add to Cart</a>  
+                        <a class="btn btn-primary" target=_blank href="item.php?id={$row['product_id']}">Add to Cart</a>  
                     </div>   
                     
                 </div>
@@ -61,6 +61,26 @@ function get_products() {
         echo $product;
 
         
+    }
+}
+
+
+function get_categories() {
+
+$query = query("SELECT * FROM categories");
+confirm($query);
+
+    while($row = fetch_array($query)) {
+    
+        $category_links = <<<DELIMETER
+
+        <a href='category.php?id={$row['cat_id']}' class='list-group-item'>{$row['cat_title']}</a>
+
+DELIMETER;
+
+echo $category_links;
+
+
     }
 }
 
